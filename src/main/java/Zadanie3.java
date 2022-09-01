ec578595b3850fe0277d19101bba66e89e7222f4
KubaTworek <kubatvorek@gmail.com>
2022-09-02 01:01:01 +0200

    init

---
+++ src/main/java/Zadanie3.java
@@ -0,0 +1,16 @@
+public class Zadanie3 {
+    /**
+     * Z publicznego API: https://jsonplaceholder.typicode.com pobierz wszystkie zapisane posty znajdujące się pod adresem
+     * https://jsonplaceholder.typicode.com/posts tak by po wywołaniu metody getPosts() można było wypisać wszystkie elementy w konsoli,
+     * podobnie jak wypisuje je przeglądarka po wejściu w link.
+     *
+     * Można skorzystać z dowolnych sposobów pobierania danych z API dostępnych dla języka Java.
+     * */
+    public static String getPosts() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPosts());
+    }
+}
